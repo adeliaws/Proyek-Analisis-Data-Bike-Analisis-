24ce28ab10e75e234045f211488e4e0e88b7022e
adeliaws <160885162+adeliaws@users.noreply.github.com>
2024-03-07 14:18:09 +0700

    Update (Streamlit) Proyek_Analisis_Data_Adelia_Wahyu_S.py

--- dashboard/(Streamlit) Proyek_Analisis_Data_Adelia_Wahyu_S.py
+++ dashboard/(Streamlit) Proyek_Analisis_Data_Adelia_Wahyu_S.py
@@ -9,16 +9,12 @@ st.title('Proyek Analisis Data')
 st.write('Adelia Wahyu Suryandari')
 
 day_df = pd.read_csv("day.csv", delimiter=",")
-st.dataframe(day_df)
-hour_df = pd.read_csv("hour.csv", delimiter=",")
-st.dataframe(hour_df)
 
 #Pertanyaan 1
 
 #Trend Harian
 trend_harian = day_df.groupby('dteday')['cnt'].mean()
-#Trend Jam per Jam
-trend_jam = hour_df.groupby('hr')['cnt'].mean()
+
 #Grafik "Tren Peminjaman Sepeda Harian"
 st.subheader('Tren Peminjaman Sepeda Harian')
 fig_trend_harian = plt.figure(figsize=(16, 6))
@@ -28,15 +24,6 @@ plt.xlabel('Tanggal')
 plt.ylabel('Rata-rata Jumlah Peminjaman')
 plt.xticks(rotation=45)
 st.pyplot(fig_trend_harian)
-
-#Trend Peminjaman Sepeda per Jam
-st.subheader('Tren Peminjaman Sepeda per Jam')
-fig_trend_jam = plt.figure(figsize=(12, 6))
-sns.lineplot(x=trend_jam.index, y=trend_jam.values)
-plt.title('Tren Peminjaman Sepeda per Jam')
-plt.xlabel('Jam dalam Sehari')
-plt.ylabel('Rata-rata Jumlah Peminjaman')
-st.pyplot(fig_trend_jam)
 
 #Pertanyaan 2
 seasonal_avg = day_df.groupby(['season', 'dteday'])[['casual', 'registered', 'cnt']].mean().reset_index()
